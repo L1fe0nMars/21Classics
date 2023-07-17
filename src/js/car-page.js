04fe8cd28c7ec9e515b50532866d3c1633c7a417
L1fe0nMars <jkama005@gmail.com>
2023-07-17 16:06:27 -0400

    Added pop-up gallery

--- src/js/car-page.js
+++ src/js/car-page.js
@@ -1,22 +1,24 @@
-const SWIPER_SLIDES = document.getElementsByClassName("swiper-slide")
+const MAIN_IMG = document.getElementsByClassName("main-img")
+const SWIPER_CONTAINER = document.getElementsByClassName("swiperContainter")
+const MODAL = document.getElementById("modal")
+const CLOSE_BUTTON = document.getElementById("close")
 
 let bottomSwiper = new Swiper(".bottomSwiper", {
     loop: false,
     spaceBetween: 10,
-    slidesPerView: 2.5,
+    slidesPerView: 2,
     breakpoints: {
         640: {
-          slidesPerView: 4.5,
+          slidesPerView: 4,
         },
         1024: {
-          slidesPerView: SWIPER_SLIDES.length / 2,
+          slidesPerView: 6,
         },
     },
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-		dynamicBullets: true,
-	},
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     freeMode: true,
     watchSlidesProgress: true,
 })
@@ -27,15 +29,70 @@ let mainSwiper = new Swiper(".swiperContainer", {
     thumbs: {
       swiper: bottomSwiper,
     },
+    keyboard: {
+        enabled: true,
+    },
+})
+/*
+let modalBottomSwiper = new Swiper(".modalBottomSwiper", {
+    loop: false,
+    spaceBetween: 10,
+    slidesPerView: 2,
+    breakpoints: {
+        640: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 6,
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    freeMode: true,
+    watchSlidesProgress: true,
+})
+*/
+let modalSwiper = new Swiper(".modalSwiper", {
+    loop: true,
+    spaceBetween: 10,
+    grabCursor: true,
+    /*
+    thumbs: {
+      swiper: modalBottomSwiper,
+    },*/
+    keyboard: {
+        enabled: true,
+    },
 })
 
-console.log(SWIPER_SLIDES[0].getBoundingClientRect().height)
-console.log(SWIPER_SLIDES[2].getBoundingClientRect().height)
+function openModal(slideIndex) {
+    MODAL.setAttribute("style", "display:block !important")
+    modalSwiper.slideToLoop(slideIndex)
+    BODY.style.overflow = "hidden"
+}
 
-window.addEventListener('resize', function() {
-	for (let i = 0; i < SWIPER_SLIDES.length / 2; i++) {
-		SWIPER_SLIDES[i].getBoundingClientRect().height = SWIPER_SLIDES[0].getBoundingClientRect().height
-		console.log(SWIPER_SLIDES[0].getBoundingClientRect().height)
-		console.log(SWIPER_SLIDES[2].getBoundingClientRect().height)
-	}
+function closeModal() {
+    MODAL.setAttribute("style", "display:none !important")
+    BODY.style.overflow = "auto"
+}
+
+function isModalOpen() {
+    return (MODAL.style.display == "block" ? true : false)
+}
+
+
+for (let i = 0; i < MAIN_IMG.length; i++) {
+    MAIN_IMG[i].addEventListener("click", () => {
+        openModal(i)
+    })
+}
+
+CLOSE_BUTTON.addEventListener("click", closeModal)
+
+document.addEventListener("keydown", (event) => {
+    if (isModalOpen() && event.key === "Escape") {
+        closeModal()
+    }
 })
