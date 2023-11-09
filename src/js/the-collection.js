a38cc43f57481840e9ebbd28aa66f2743b004c31
L1fe0nMars <jkama005@gmail.com>
2023-11-09 00:47:57 -0500

    Improved look for tablets

--- src/js/the-collection.js
+++ src/js/the-collection.js
@@ -3,9 +3,8 @@
 const UNSOLD_TAB = document.getElementById("unsold-tab");
 const SOLD_TAB = document.getElementById("sold-tab");
 const CARD = document.getElementsByClassName("card");
-const NAME = document.getElementsByClassName("name");
 
-/*console.log(NAME[0].parentElement.parentElement.parentElement);*/
+console.log(CARD[0].querySelector(".name").innerHTML);
 
 UNSOLD_TAB.addEventListener("click", () => {
     for (let i = 0; i < CARD.length; i ++) {
@@ -27,13 +26,6 @@ UNSOLD_TAB.addEventListener("click", () => {
     if (SOLD_TAB.classList.contains("selected")) {
         SOLD_TAB.classList.toggle("selected");
     }
-    /*
-    for (let i = 0; i < NAME.length; i++) {
-        let str = NAME[i].innerHTML;
-
-        console.log(str.substring(0, str.indexOf(' ')));
-    }
-    */
 });
 
 SOLD_TAB.addEventListener("click", () => {
